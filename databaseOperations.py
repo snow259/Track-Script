72c01700ef819780bddaa0bf894511c564f42cec
snow259 <johann.abraham7@gmail.com>
2020-08-06 22:50:43 +0530

    Input, delete, modify, input time, calculate duration, are all functional

--- databaseOperations.py
+++ databaseOperations.py
@@ -1,5 +1,6 @@
 import sqlite3
 import os
+import datetime as dt
 
 filePath = os.path.realpath(__file__)
 fileDirectory = os.path.dirname(filePath)
@@ -18,23 +19,85 @@ def checkForDatabase():
 	if dirList.count('mainDatabase.db') == 0:
 		database = sqlite3.connect(databasePath)
 		cursor = database.cursor()
-		cursor.execute('CREATE TABLE Games (id INTEGER PRIMARY KEY, name TEXT NOT NULL, startTime TIMESTAMP, endTime TIMESTAMP, duration TIMESTAMP)')
+		cursor.execute('CREATE TABLE Games (id INTEGER PRIMARY KEY, name TEXT NOT NULL, startTime TIMESTAMP, endTime TIMESTAMP, duration TEXT)')
 		database.close()
 
+#Todo: add code for multiple open session repair, with option to select session by id, then delete or close it with entered time
 #Checks for an open session
-# def checkOpenSessions():
+def checkOpenSessions():
+	database = sqlite3.connect(databasePath)
+	database.row_factory = sqlite3.Row
+	cursor = database.cursor()
+	cursor.execute('SELECT id, name, startTime FROM Games WHERE endTime IS ?', (None, ))
+	rows = cursor.fetchall()
+	# for row in rows:
+	# 	print(str(row['id']) + ' ' + str(row['name']) + ' ' + str(row['startTime']))
+	database.close()
+
+	return rows
+
+def checkDurations():
+	database = sqlite3.connect(databasePath)
+	database.row_factory = sqlite3.Row
+	cursor = database.cursor()
+	checkDurationString = 'SELECT id, startTime, endTime FROM Games WHERE startTime IS NOT NULL AND endTime IS NOT NULL AND duration IS NULL'
+	cursor.execute(checkDurationString)
+	rows = cursor.fetchall()
+	database.close()
+
+	return rows
+
+def returnDatabaseContents():
+	database = sqlite3.connect(databasePath)
+	database.row_factory = sqlite3.Row
+	cursor = database.cursor()
+	cursor.execute('SELECT * FROM Games')
+	rows = cursor.fetchall()
+	database.close()
+
+	return rows
 
 #Writes session to database
-# def writeSession():
+def writeSession(rowId, name, startTime, endTime, duration):
+	insertString = 'INSERT INTO Games VALUES(?, ?, ?, ?, ?)'
+	database = sqlite3.connect(databasePath)
+	cursor = database.cursor()
+	cursor.execute(insertString, (rowId, name, startTime, endTime, duration))
+	database.commit()
+	database.close()
 
-#Edits session already written, scope of editability yet to be determined
-# def editSession():
+#Closes session in database
+def closeSession(endTime):
+	closeString = 'UPDATE Games SET endTime = ? WHERE endTime IS NULL'
+	database = sqlite3.connect(databasePath)
+	cursor = database.cursor()
+	cursor.execute(closeString, (endTime, ))
+	database.commit()
+	database.close()
 
-#Deletes open sessions
-# def deleteSession():
+#Deletes specified sessions
+def deleteSession(rowId):
+	deleteString = 'DELETE FROM Games WHERE id IS ?'
+	database = sqlite3.connect(databasePath)
+	cursor = database.cursor()
+	cursor.execute(deleteString, (rowId, ))
+	database.commit()
+	database.close()
+
+def writeDuration(rowId, duration):
+	print(rowId)
+	print(duration)
+	writeDurationString = 'UPDATE Games SET duration = ? WHERE id IS ?'
+	database = sqlite3.connect(databasePath)
+	cursor = database.cursor()
+	cursor.execute(writeDurationString, (duration, rowId))
+	database.commit()
+	database.close()
 
 #Creates a backup of the database
 # def backupDatabase():
 
 checkForDataFolder()
 checkForDatabase()
+# writeSession(None, 'Test3', dt.datetime.now(), None, None)
+checkOpenSessions()
--- track.py
+++ track.py
@@ -1,36 +1,148 @@
 import datetime as dt
 import calendar
-# import databaseOperations as dataops
+import databaseOperations as dataops
 
-# operatingDirectory = 
+#Checks database for open session of game, attempts repair if more than one open session is discovered
+def checkSession():
+	rows = dataops.checkOpenSessions()
+	if len(rows) == 0:
+		checkDuration()
+		inputStart()
 
-#Checks if database exists, creates it if it does not
+	if len(rows) == 1:
+		row = rows[0]
+		rowId = row['id']
+		game = row['name']
+		startTime = row['startTime']
+		print('Open session:\n' + str(game) + ' ' + str(startTime))
+
+		inputEnd(rowId)
+
+	if len(rows) > 1:
+		print('Multiple open sessions found:')
+		for row in rows:
+			rowId = row['id']
+			game = row['name']
+			startTime = row['startTime']
+			outString = str(rowId) + ' ' + str(game) + ' ' + str(startTime)
+			print(outString)
+		# print('Repair options: close, delete')
+		repairOption = ''
+		while repairOption != 'close' and repairOption != 'delete':
+			repairOption = input('Repair options: close, delete\n')
+
+		# if repairOption == 'close':
+
+		if repairOption == 'delete':
+			deleteSession()
+
+def checkDuration():
+	rows = dataops.checkDurations()
+	if len(rows) > 0:
+		for row in rows:
+			rowId = row['id']
+			startTime = row['startTime']
+			endTime = row['endTime']
+			calculateDuration(rowId, startTime, endTime)
 
 
 #Takes input from user, reads current time
-def inputGame():
-	newGame = input('Enter game: ')
-	# gameTime = stringDateTime(dt.datetime.now(), round = True)
-	gameTime = roundTime(dt.datetime.now())
+def inputStart():
+	inputString = input('Enter game: ')
+	dateAndTimeRaw = dt.datetime.now()
+	gameTime = roundTime(dateAndTimeRaw)
 	gameTime = removeSeconds(gameTime)
-	print(gameTime)
 
-#Checks database for open session of game
-# def checkSession():
+	if inputString == 'modify':
+		listSessions()
+		modify()
+	elif inputString == 'list':
+		listSessions()
+	elif inputString == 'exit':
+		pass
+	else:
+		dataops.writeSession(None, inputString, gameTime, None, None)
 
-#Writes game and time to database
-# def writeToDB(game):
+	if inputString != 'exit':
+		checkSession()
 
-#Converts datetime object to dictionary, done to allow changing of values
-def stringDateTime(dateAndTimeRaw, round):
-	if round == True:
-		dateAndTimeRaw = roundTime(dateAndTimeRaw)
+def inputEnd(rowId):
+	choice = ''
+	while choice != 'close' and  choice != 'delete' and choice != 'input':
+		choice = input('Options: close, delete, input\n')
+	endTime = roundTime(dt.datetime.now())
+	endTime = removeSeconds(endTime)
 
-	dateString = dt.date.isoformat(dateAndTimeRaw)
-	timeString = dt.datetime.strftime(dateAndTimeRaw, '%H:%M:00')
-	dateTimeString  = dateString + ' ' + timeString
+	if choice == 'close':
+		dataops.closeSession(endTime)
+		checkSession()
+	elif choice == 'delete':
+		deleteSession(rowId)
+	elif choice == 'input':
+		userInputEndTime()
 
-	return dateTimeString
+def deleteSession(rowId = None):
+	if rowId == None:
+		rowIdRaw = input('Enter ids to delete: ')
+		rowIds = rowIdRaw.split()
+		for rowId in rowIds:
+			dataops.deleteSession(int(rowId))
+	else:
+		dataops.deleteSession(rowId)
+
+	checkSession()
+
+def userInputEndTime():
+	inputCorrect = False
+	while inputCorrect == False:
+		userEndTime = input('Enter end time in format: YYYY-MM-DD HH:MM:SS\n')
+		isThisRightString = 'Entered time is: ' + userEndTime + '. Is this satisfactory? (y/n)'
+		isThisRight = input(isThisRightString)
+		if isThisRight == 'y':
+			inputCorrect = True
+
+	userEndTime = userEndTime.rstrip()
+	print(userEndTime)
+	userEndTime = dt.datetime.strptime(userEndTime, '%Y-%m-%d %H:%M:%S')
+	userEndTime = roundTime(userEndTime)
+	endTime = removeSeconds(userEndTime)
+	dataops.closeSession(endTime)
+	checkSession()
+
+def calculateDuration(rowId, startTime, endTime):
+	startTime = dt.datetime.strptime(startTime, '%Y-%m-%d %H:%M:%S')
+	endTime = dt.datetime.strptime(endTime, '%Y-%m-%d %H:%M:%S')
+	duration = str(endTime - startTime)
+	# duration = dt.datetime.strftime(duration, '%H:%M:%S')
+	dataops.writeDuration(rowId, duration)
+
+def listSessions():
+	rows = dataops.returnDatabaseContents()
+	if len(rows) == 0:
+		print('No sessions found in database')
+
+	if len(rows) > 0:
+		for row in rows:
+			rowId = str(row['id'])
+			name = str(row['name'])
+			startTime = str(row['startTime'])
+			endTime = str(row['endTime'])
+			duration = str(row['duration'])
+			outString = ''
+			listOfElements = [rowId, name, startTime, endTime, duration]
+			for element in listOfElements:
+				outString = outString + element + '	'
+			print(outString)
+
+def modify():
+	action = ''
+	while action != 'delete' and action != 'modify' and action != 'cancel':
+		action = input('Select action: delete, modify, cancel\n')
+	if action == 'cancel':
+		checkSession()
+
+	if action == 'delete':
+		deleteSession()
 
 #Returns number of days in requested month
 def daysInMonth(dateAndTime):
@@ -48,6 +160,7 @@ def roundTime(dateAndTimeRaw):
 
 	return dateAndTimeRaw
 
+#Gets rid of the seconds part of time, as second level accuracy is not used
 def removeSeconds(gameTime):
 	second = gameTime.second
 	microsecond = gameTime.microsecond
@@ -56,7 +169,10 @@ def removeSeconds(gameTime):
 
 	return gameTime
 
-inputGame()
+def exit():
+	print('Exiting')
+
+checkSession()
 
 #Input game -> stuff to write thing to database
 #			-> function to input end time of game, as only one game can be played at a time
