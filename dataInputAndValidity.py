ed2d5eb1eab3cb1cbf616a8b9bc32365cccb2c7f
snow259 <67005128+snow259@users.noreply.github.com>
2024-03-02 14:57:36 -0600

    Fixed crash when duration is None (upon reboot for example)

--- dataInputAndValidity.py
+++ dataInputAndValidity.py
@@ -220,19 +220,20 @@ def changeDurationStringFormat(rows, cols):
 		rows[i] = dict(rows[i])
 
 		for col in cols:
-			split = rows[i][col].split(':')
+			if rows[i][col] is not None:
+				split = rows[i][col].split(':')
 
-			# Formatting to hh:mm
-			# for j in range(2):
-			# 	if len(split[j]) == 1:
-			# 		split[j] = '0' + split[j]
+				# Formatting to hh:mm
+				# for j in range(2):
+				# 	if len(split[j]) == 1:
+				# 		split[j] = '0' + split[j]
 
-			# Formatting to h:mm
-			if int(split[0]) < 10:
-				split[0] = str(int(split[0]))
-			if len(split[1]) == 1:
-				split[1] = '0' + split[1]
+				# Formatting to h:mm
+				if int(split[0]) < 10:
+					split[0] = str(int(split[0]))
+				if len(split[1]) == 1:
+					split[1] = '0' + split[1]
 
-			rows[i][col] = split[0] + ':' + split[1]
+				rows[i][col] = split[0] + ':' + split[1]
 
 	return rows
